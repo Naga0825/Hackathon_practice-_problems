08463518e8800b91debcad98bca231683a0ce6d2
Nagalakshmi R <naganandhini475@gmail.com>
2026-03-29 19:32:03 +0530

    Create RemoveDuplicatesreturnLength.java

---
+++ RemoveDuplicatesreturnLength.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class RemoveDuplicates{
+    public static int Remove(int[] arr){
+        int i =0;
+        int j =0;
+        for(i =0;i<arr.length;i++){
+            if(arr[i]!=arr[j]){
+                j++;
+                arr[j]=arr[i];
+            }
+        }
+    return j+1;
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,2,3,3,4,4,5,6};
+        int result = Remove(arr);
+        System.out.println(result);
+    }
+}
