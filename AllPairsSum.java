3f6d89dee8260ccb956435187f4bf2bd491556d1
Nagalakshmi R <naganandhini475@gmail.com>
2026-03-30 18:43:14 +0530

    Create AllPairsSum.java

---
+++ AllPairsSum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class AllPairs{
+    public static void main(String[] args){
+        int [] arr = {1,2,3,4,5};
+        for(int i =0;i<arr.length;i++){
+            for(int j =i+1;j<arr.length;j++){
+                int sum =0;
+                sum+=arr[i]+arr[j];
+                System.out.println(sum);
+            }
+            System.out.println();
+        }
+    }
+}
